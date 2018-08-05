6d0cfb352affc9bfb9f76934cc519a19060be07d
DESKTOP-1L38NVA\Nilesh Devdas <nilesh.devdas@gmail.com>
2018-08-05 10:25:12 +0530

    Base Project

---
+++ app4/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace app4.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public String getMessage() {
+
+            return "Helllo";
+        }
+
+    }
+}
---
+++ app4/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace app4.Models
+{
+    public class Class1
+    {
+        public String message { get; set; }
+    }
+}
